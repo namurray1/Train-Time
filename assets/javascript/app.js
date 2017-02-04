b5c486d116950f40efaf242c2cdf3f60a258b49f
Alex Murray <namurray1@gmail.com>
2017-02-04 03:30:38 -0500

    Updated html and CSS. Firebase is firing, but still need to correct some of the code to get it to work with momentjs.

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,45 +1,86 @@
-// Initialize Firebase
-var config = {
-    apiKey: "AIzaSyDWS07PYdyLmizC55bK-AQHVmnTAu5e2vE",
-    authDomain: "train-time-855af.firebaseapp.com",
-    databaseURL: "https://train-time-855af.firebaseio.com",
-    storageBucket: "train-time-855af.appspot.com",
-    messagingSenderId: "777346639577"
-};
-firebase.initializeApp(config);
-
 var database = firebase.database();
+var trainCommin = database.ref('/Trains')
 var trainName = "";
 var destination = "";
 var nextArrival = "";
-var minutesAway = "";
+var minutesAway = 0;
+var firstTrainTime = "";
+var frequency = 0;
+
+$(document).ready(function() {
 
 
-//Capture user inputs and store into variables
-$("#runSearch").on("click", function() {
-    trainName = $("#trainName").val().trim();
-    destination = $("#destination").val().trim();
-    nextArrival = $("#nextArrival").val().trim();
-    minutesAway = $("#minutesAway").val().trim();
+    database.ref().set({
 
-    database.ref().push({
-        trainName: trainName,
-        destination: destination,
-        nextArrival: nextArrival,
-        minutesAway: minutesAway,
-        dateAdded: firebase.database.ServerValue.TIMESTAMP
+
+        train: {
+            destination: destination,
+            trainName: trainName,
+            nextArrival: nextArrival,
+            minutesAway: minutesAway,
+            firstTrainTime: firstTrainTime,
+            frequency: frequency,
+        }
+
+
+
     });
 
+});
+//Capture user inputs and store into variables
+$("#runSearch").on("click", function() {
 
+    trainName = $("#trainName").val().trim();
+    destination = $("#destination").val().trim();
+    frequency = $("#frequency").val().trim();
+    // nextArrival = $("#nextArrival").val().trim();
+    // minutesAway = $("#minutesAway").val().trim();
+    firstTrainTime = $("#firstTrainTime").val()
+
+    // console.log(trainName)
+    // console.log(destination)
+    // console.log(frequency)
+    // console.log(firstTrainTime)
+
+
+    database.ref().update({
+
+        train: {
+            destination: destination,
+            trainName: trainName,
+            nextArrival: nextArrival,
+            minutesAway: minutesAway,
+            firstTrainTime: firstTrainTime,
+            frequency: frequency,
+        }
+        // dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+
+    database.ref().on("child_added", function(childSnapshot) {
+
+
+        // console.log(childSnapshot.val());
+        // console.log(childSnapshot.val().destination)
+        // console.log(childSnapshot.val().trainName)
+        // console.log(childSnapshot.val().nextArrival)
+        // console.log(childSnapshot.val().minutesAway)
+        // console.log(childSnapshot.val().firstTrainTime)
+        // console.log(childSnapshot.val().frequency)
+
+        $(".table").append("<td>" + childSnapshot.val().trainName + "</td>");
+        $(".table").append("<td>" + childSnapshot.val().destination + "</td>");
+        $(".table").append("<td>" + childSnapshot.val().firstTrainTime + "</td>");
+        $(".table").append("<td>" + childSnapshot.val().frequency + "</td>");
+
+
+        return false
+
+    });
+    return false
 
 });
-database.ref().on("child_added", function(childSnapshot) {
-    console.log(childSnapshot.val().trainName);
-    console.log(childSnapshot.val().destination);
-    console.log(childSnapshot.val().nextArrival);
-    console.log(childSnapshot.val().minutesAway);
 
-});
+
 
 // Capture Button Click
 $("#runSearch").on("click", function(event) {
@@ -48,11 +89,12 @@ $("#runSearch").on("click", function(event) {
     // Replaces the content in the "currentTrainSchedule" div
     $("#currentTrainSchedule").empty();
 
+
     // Output all of the new information into the Current Train Schedule section
-    $("#currentTrainSchedule").append("<h1>" + trainName);
-    $("#currentTrainSchedule").append("<h4>" + destination);
-    $("#currentTrainSchedule").append("<h4>" + nextArrival);
-    $("#currentTrainSchedule").append("<h4>" + minutesAway);
+    $("#currentTrainSchedule").append("<td>" + trainName + "</td>");
+    $("#currentTrainSchedule").append("<td>" + destination + "</td>");
+    $("#currentTrainSchedule").append("<td>" + nextArrival + "</td>");
+    $("#currentTrainSchedule").append("<td>" + minutesAway + "</td>");
 
     // Clear sessionStorage
     sessionStorage.clear();
@@ -66,7 +108,7 @@ $("#runSearch").on("click", function(event) {
 
 // By default display the content from sessionStorage
 $("#currentTrainSchedule").empty();
-$("#currentTrainSchedule").append("<h2>" + sessionStorage.getItem("trainName"));
-$("#currentTrainSchedule").append("<h4>" + sessionStorage.getItem("destination"));
-$("#currentTrainSchedule").append("<h4>" + sessionStorage.getItem("nextArrival"));
-$("#currentTrainSchedule").append("<h4>" + sessionStorage.getItem("minutesAway"));
+$("#currentTrainSchedule").append("<td>" + sessionStorage.getItem("trainName"));
+$("#currentTrainSchedule").append("<td>" + sessionStorage.getItem("destination"));
+$("#currentTrainSchedule").append("<td>" + sessionStorage.getItem("nextArrival"));
+$("#currentTrainSchedule").append("<td>" + sessionStorage.getItem("minutesAway"));
